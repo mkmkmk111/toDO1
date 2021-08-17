6136a6917531c1072bac0ac9ec4ec336717a39e9
mkmkmk111 <89062084+mkmkmk111@users.noreply.github.com>
2021-08-17 15:05:27 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        addText: '',
+        list: [],
+        uniqueKey: 0,
+    },
+
+    methods: {
+        addToDo() {
+            if (this.addText) {
+                this.list.unshift({
+                    'text': this.addText,
+                    'id': ++this.uniqueKey
+                });
+                this.addText = '';  //入力値初期化
+            }
+        },
+        deleteToDo(id) {
+            var deleteIndex = '';
+            var check = confirm('本当に削除しますか？');
+            if (check === true) {    //アラートでOKが押下されたら
+                this.list.some(function (value, index) {
+                    if (value.id === id) {
+                        deleteIndex = index;
+                    }
+                });
+                this.list.splice(deleteIndex, 1);
+            }
+        }
+       
+    }
+});
